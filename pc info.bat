a7a0d9689f4d52f27b1598b20c11c14e9cc81045
xaviernj80 <33666681+xaviernj80@users.noreply.github.com>
2018-03-05 16:20:58 -0500

    Add files via upload

--- pc info.bat
+++ pc info.bat
@@ -4,16 +4,16 @@
 echo Checking your system info, Please wait...
 systeminfo | findstr /c:"Host Name"
 systeminfo | findstr /c:"Domain"
-systeminfo | findstr /c:"User"
+systeminfo | findstr /c:"Logon Server"
+whoami
 systeminfo | findstr /c:"OS Name"
 systeminfo | findstr /c:"OS Version"
-systeminfo | findstr /c:"Logon Server"
+systeminfo | findstr /c:"Time Zone"
 systeminfo | findstr /c:"System Manufacturer"
 systeminfo | findstr /c:"System Model"
 systeminfo | findstr /c:"System type"
 systeminfo | findstr /c:"Total Physical Memory"
-ipconfig | find /i "IPv4"
-
+ipconfig | find /i "Ethernet"
 echo Completed! Thank you!
 
 pause
